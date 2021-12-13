474256d207f04b22383b5f18b2e2cb505ed5c0d1
Polarking8 <57197175+Polarking8@users.noreply.github.com>
2021-12-12 17:21:27 -0700

    added javadoc for new acc markt

--- src/NewAccountMarketing.java
+++ src/NewAccountMarketing.java
@@ -1,4 +1,10 @@
 public class NewAccountMarketing {
+    /**
+     * For each of these months, take the initbalance and interest and do math with it
+     * @param initBalance
+     * @param interestRate
+     * @return
+     */
     public static double getMonthOneBalance(double initBalance, double interestRate){
         return (initBalance * (1 + (interestRate / 1200)));
     }
